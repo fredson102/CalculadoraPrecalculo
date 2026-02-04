a2f428ad9189d9dfdd9e03721f83db65e05dd8fc
fredson102 <69481135+fredson102@users.noreply.github.com>
2026-02-03 21:39:42 -0600

    Eliminar tests OCR obsoletos

--- test_tesseract.py
+++
@@ -1,5 +0,0 @@
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-print('tesseract_cmd:', pytesseract.pytesseract.tesseract_cmd)
-from pytesseract import get_tesseract_version
-print('tesseract version from pytesseract:', get_tesseract_version())
